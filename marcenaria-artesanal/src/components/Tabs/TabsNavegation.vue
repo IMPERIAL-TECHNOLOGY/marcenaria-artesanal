2fb76df6e91013556ce553411c1f95a7233592b5
danieldificil <daniel.araujo@ideabox-it.com>
2022-11-03 05:32:37 -0300

    [FEAT] add favicon

---
+++ marcenaria-artesanal/src/components/Tabs/TabsNavegation.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="test">
+    <v-tabs
+        background-color="transparent"
+        color="white"
+        height="30"
+    >
+      <div class="header" v-for="(tab, i) in tabs" :key="i">
+        <v-tab
+            v-bind="$props"
+            style="color: white; font-size: large"
+            right
+        >
+          {{ tab.name }}
+        </v-tab>
+      </div>
+    </v-tabs>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TabsNavegation',
+  props: {
+    tabs: Array
+  },
+}
+</script>
+<style scoped>
+test {
+  width: 100%;
+}
+</style>
