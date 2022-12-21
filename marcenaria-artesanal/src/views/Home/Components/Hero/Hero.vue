45fc2a3b06d118f3ba713afdf972abc27e5730d9
danieldificil <daniel.araujo@ideabox-it.com>
2022-12-20 21:51:02 -0300

    [WIP] IPHONE VIEW BUG...

--- marcenaria-artesanal/src/views/Home/Components/Hero/Hero.vue
+++ marcenaria-artesanal/src/views/Home/Components/Hero/Hero.vue
@@ -240,7 +240,7 @@ export default {
 .hero-container {
   height: 100vh;
   background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
-    url("../../../../assets/images/contato-pic.WebP") no-repeat fixed center;
+    url("../../../../assets/images/contato-pic.WebP") no-repeat center;
   background-size: cover;
 }
 .hero-open-menu-icon-mobile {
